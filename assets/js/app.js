3b8f7ef01c10e71b34f19f6ec5369154aaf242e2
Ethan Kwon <46038779+EthanKwon@users.noreply.github.com>
2020-03-30 22:16:23 +0900

    skill layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,52 @@
 //document
 
+const scrollShow = document.querySelector(".scroll_show");
+
 const scrollContent = document.querySelector(".scroll_content");
 
+//intro
+
 const sec1Fixed = document.querySelectorAll(".sec1_fixed");
-console.log(sec1Fixed.sticky);
 const sec1Img = document.querySelector(".section1_img1");
+
+//about
+
+const about = document.querySelector(".about");
+const aboutText = document.querySelector(".about_desc");
+
+//skill part
+const skill = document.querySelector(".skill");
+const skillTitle = skill.querySelector(".skill_title");
 
 //scroll effect
 
 const scrollTl = gsap.timeline();
 
 window.addEventListener("scroll", () => {
-  const wScroll = Math.round(window.scrollY);
+  const wScroll = Math.round(this.scrollY);
   const wScrollSlow = slow => wScroll / slow;
-  console.log(wScroll);
+  scrollShow.innerHTML = `${wScroll}`;
+
+  //skill Scrolling
+
+  // scroll
   scrollContent.style.transform = `translate3d(0,${-wScroll}px,0)`;
 
+  //intro scrolling
   sec1Fixed.forEach(fixed => {
     fixed.style.transform = `translate3d(0,${2 * wScroll}px,0)`;
   });
   sec1Img.style.transform = `translate3d(0,${wScrollSlow(10)}px,0)`;
+
+  //qbout scrolling
+  if (wScroll >= 200 && wScroll < 900) {
+    const aboutTextPos = aboutText.getBoundingClientRect().top - wScroll;
+    aboutText.style.transform = `translate3d(0,${aboutTextPos / 15}px,0)`;
+  }
+  //skill scrolling
+  if (wScroll >= 600 && wScroll < 1600) {
+    const skillTitlePos = skillTitle.getBoundingClientRect().top;
+    skillTitle.style.transform = `translate3d(${-skillTitlePos /
+      5}px,${-skillTitlePos / 5}px,0)`;
+  }
 });
